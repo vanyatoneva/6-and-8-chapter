b78943a87ceacda6bda7229613b07d6b4fce80e1
vanyatoneva <vanyattoneva@gmail.com>
2019-05-14 17:50:37 +0300

    Update ll_func.h

--- LinkedList/LinkedList/ll_func.h
+++ LinkedList/LinkedList/ll_func.h
@@ -22,6 +22,16 @@ void sortedInsert(Listptr* head, Listptr node);    //inserts the node in the cor
 void insertSort(Listptr* head);                    //sorts the list in icreasing order
 void append(Listptr* appTo, Listptr* toApp);  //Appends the secpond to first, then deletes the second
 void frontBackSplit(Listptr head, Listptr* front, Listptr* back);   //splits the list at half : at front keep the first, at back second
+void removeDuplicates(Listptr* head);          //removes duplicates in sorted linked list
+void moveNode(Listptr* to, Listptr* from);     // move the first node from second list to the begin of the first
+void alternatingSplit(Listptr head, Listptr* first, Listptr* sec); //even elem. goes to the first, odd in the second - new lists are reversed !!
+void alternatingSplit2(Listptr head, Listptr* first, Listptr* sec); // this one makes the lists in the same order like the orig.
+Listptr shuffleMerge(Listptr f, Listptr s); //takes the two lists and makes it one - 0th of(1), 0th of(2), 1st of(1), 1st of(2) - in this order
+Listptr sortedMerge(Listptr f, Listptr s);   // takes two sorted lists, and splits them into one sorted list
+void mergeSort(Listptr* head);     //sort recursively
+Listptr sortedIntersection(Listptr f, Listptr s); //makes new list, representing the intersection of the two given. Given lists must be in increasing order
+void reverse(Listptr* head); //reverses list
+void reverseRec(Listptr* head); //reverses list recursively
 
 #endif // ! LINLIST
 
